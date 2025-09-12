e0e5ff3528322b8ace2db940032c88c074f22051
wei-1-1 <81368369+wei-1-1@users.noreply.github.com>
2025-09-12 23:11:53 +0800

    Update index.js
    freecode camp backend api project: personal solution for timestamp api exercises

--- index.js
+++ index.js
@@ -1,32 +1,75 @@
 // index.js
-// where your node app starts
+
 
 // init project
-var express = require('express');
-var app = express();
+import express from 'express';
+import cors from "cors";
+import formateTimeMid from "./timeFormatterMid.js";
 
-// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
-var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+const app = express();
 
-// http://expressjs.com/en/starter/static-files.html
+
+
+app.use(cors({optionsSuccessStatus: 200}));
+
+
 app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
+    res.sendFile(__dirname + '/views/index.html');
 });
 
 
-// your first API endpoint... 
+
 app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+    res.json({greeting: 'hello API'});
 });
 
+app.get("/api/",
+    (req, res, next) => {
+        req.timestamp = Date.now();
+        next();
+    },
+    formateTimeMid,
+    (req, res) => {
 
+        res.send({
+            unix: parseInt(req.timestamp, 10),
+            ...req.formattedTime
+        });
+    }
+)
+
+app.get(/^\/api\/(\w+)$/,//需要括号才能识别参数……
+    (req, res, next) => {
+        req.timestamp = req.params[0];
+       // console.log(`Route /api/\w+: req.params[0] = ${req.params[0]}, req.timestamp = ${req.timestamp}`);
+        next();
+    },
+    formateTimeMid,
+    (req, res) => {
+        if (req.formattedTime.error) {
+            return res.status(400).send(req.formattedTime);
+        }
+        res.send(req.formattedTime);
+    }
+);
+
+app.get("/api/:date?",
+    formateTimeMid,
+    (req, res) => {
+        if (req.formattedTime.error) {
+            return res.send(req.formattedTime);
+        }
+        res.send({
+            unix: parseInt(req.timestamp, 10),
+            ...req.formattedTime,
+        });
+    }
+);
 
 // Listen on port set in environment variable or default to 3000
-var listener = app.listen(process.env.PORT || 3000, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+const listener = app.listen(process.env.PORT || 3000, function () {
+    console.log('Your app is listening on port ' + listener.address().port);
 });
